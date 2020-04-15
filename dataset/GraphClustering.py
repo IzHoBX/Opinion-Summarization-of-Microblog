d9c8c2f0cff25574fdcb14a8fb53a1dcc4726a77
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-04-15 11:49:20 +0800

    Adds step 4 graph clustering

---
+++ dataset/GraphClustering.py
@@ -0,0 +1,20 @@
+import sknetwork.clustering as sc
+import pickle
+
+# directories = ("spacex", "brexit", "election", "isis", "nobel", "note7")
+directories = ("isis", "nobel", "note7")
+classes = ("/positive", "/negative", "/neutral")
+l = sc.Louvain()
+
+for directory in directories:
+    for c in classes:
+        meta = pickle.load(open("./" + directory + c, "rb"))
+        # [allEntities, entityIndicesByTweet, numpy.array(matrix)]
+        clustering = l.fit(meta[2]).labels_
+        print("found " + str(max(clustering)) + " for " + directory +" " + c)
+        graphClusters = []
+        for i in range(0, max(clustering)+1):
+            graphClusters.append([])
+        for i in range(0, len(clustering)):
+            graphClusters[clustering[i]].append(i)
+        pickle.dump([meta[0], meta[1], meta[2], graphClusters], open( "./"+directory+c+"ClusteredWG", "wb"))
